f34d77fa019e9a000991f1aa3e25da716fe64150
Abel Alvarez <abelalvarez89@hotmail.com>
2017-05-17 17:46:41 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# xlsx-parse-json
+# xlsx-parse-json #
+
+### Install ###
+````
+npm install xlsx-parse-json
+````
+
+### Use node ###
+
+````
+'use strict';
+var xlsxParser = require('xlsx-parse-json');
+
+function uploadFile(file) {
+	
+	var data = xlsxParser.onFileSelection(file);
+	console.log(data); //output xls json data
+}
+
+````
+
+#### Example ####
+
+Im using ng-file-upload in this example
+
+To run example, navigate to the example folder
+
+````
+npm install
+npm start
+````
+
+# html #
+````
+ <input type="file" ngf-select="vm.uploadFile($file)" ng-disabled="vm.disableUpload" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"  />
+ <pre>{{vm.data | json}}</pre>
+````
+
+# javascript #
+````
+var xlsxParser = require('xlsx-parse-json');
+
+function uploadFile(file) {
+	
+	vm.data = xlsxParser.onFileSelection(file);
+	
+}
+````
+# input file #
+````
+Name	| Lastname	| Age
+-------------------------
+test1	| node		| 4
+test2	| node		|
+test3	| node		| 9
+test4	| node		|
+
+````
+
+# output sample #
+````
+[
+  {
+    "Name": "test1",
+    "Lastname": "node",
+    "Age": 4
+  },
+  {
+    "Name": "test2",
+    "Lastname": "node"
+  },
+  {
+    "Name": "test3",
+    "Lastname": "node",
+    "Age": 9
+  },
+  {
+    "Name": "test4",
+    "Lastname": "node"
+  }
+]
+````
---
+++ index.js
@@ -0,0 +1,107 @@
+'use strict';
+var sheetJs = require('xlsx');
+var parsedXls = [];
+
+exports.onFileSelection = function(file) {
+	parsedXls = [];
+
+	var reader = new FileReader();
+
+	reader.readAsArrayBuffer(file);
+	reader.addEventListener('loadend', function() {
+		var binary = '';
+		var bytes = new Uint8Array(reader.result);
+
+		for (var i = 0; i < bytes.byteLength; i++) {
+			binary += String.fromCharCode(bytes[i]);
+		}
+
+		return onLoadEvent(binary, reader);
+	});
+
+	return getResult();
+};
+
+function getResult() {
+	return parsedXls;
+};
+
+function onFileSelection(file) {
+
+	var reader = new FileReader();
+
+	reader.readAsArrayBuffer(file);
+	reader.addEventListener('loadend', function() {
+		var binary = '';
+		var bytes = new Uint8Array(reader.result);
+
+		for (var i = 0; i < bytes.byteLength; i++) {
+			binary += String.fromCharCode(bytes[i]);
+		}
+
+		var output = onLoadEvent(binary, reader);
+
+		return output;
+	});
+
+}
+
+function onLoadEvent(binary, reader) {
+	var workbook = sheetJs.read(binary, {
+		type: 'binary'
+	});
+
+	var first_sheet_name = workbook.SheetNames[0];
+	var worksheet = workbook.Sheets[first_sheet_name];
+
+	var desired_cells = worksheet['!ref'];
+	var lastColRow = getLastRowCol(desired_cells);
+	var headers = getHeaders(worksheet, desired_cells);
+
+	for (var R = 2; R <= lastColRow; R++) {
+		var charCode = 65;
+		var element = {};
+		headers.forEach(function(header) {
+			var cellValue = worksheet[String.fromCharCode(charCode++) + R];
+			if (cellValue) {
+				element[header] = cellValue.v
+			}
+		});
+		parsedXls.push(element);
+	}
+
+	return parsedXls;
+
+}
+
+function getLastRowCol(cells) {
+	var rows = cells.split(':');
+	var lastColLetter = extractLetter(rows[1]);
+	var array = rows[1].split(lastColLetter);
+
+	return Number(array[1]);
+}
+
+function getHeaders(worksheet, desired_cells) {
+	var cells = desired_cells.split(':');
+	var cellHeader = cells[cells.length - 1];
+	var lastColLetter = extractLetter(cells[1]);
+	var charCode = 65;
+	var headers = [];
+
+	while (true) {
+		var currentCell = String.fromCharCode(charCode++);
+		var cellHeader = worksheet[currentCell + 1];
+		if (cellHeader) {
+			headers.push(cellHeader.v)
+		}
+		if (lastColLetter == currentCell) {
+			return headers;
+		}
+	}
+}
+
+function extractLetter(str) {
+	var array = str.split(/[0-9]+/);
+	return array[0];
+}
